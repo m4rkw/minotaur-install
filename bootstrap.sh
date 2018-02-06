7f929f3613fc43f99908ac79ef731e8db9bcfff5
Mark Wadham <m@rkw.io>
2018-02-06 22:31:18 +0000

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@ if [ "$USER" != "root" ] ; then
   echo "this script must be run as root."
   exit 1
 fi
+yum -y install epel-release
 yum -y install python-pip
 yum -y install git
 pip install ansible
+git clone https://github.com/m4rkw/minotaur-install.git
