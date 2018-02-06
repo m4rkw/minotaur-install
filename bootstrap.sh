9fc35ace798aedd847c283b60b7c3e3394531b30
Mark Wadham <m@rkw.io>
2018-02-06 22:34:14 +0000

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,5 @@ yum -y install python-pip
 yum -y install git
 pip install ansible
 git clone https://github.com/m4rkw/minotaur-install.git
+cd minotaur-install
+ansible-playbook -i 'localhost,' -c local playbooks/server.yml
